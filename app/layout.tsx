861c49a3e0e38402db9607fa9ab8e17ea23ca7e9
Neno Arisma <nenoarisma@gmail.com>
2024-11-08 08:54:40 +0700

    chore: update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 import "@/assets/styles/global.css";
 
 const inter = Inter({
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
+        <Header />
         <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           {children}
         </main>
